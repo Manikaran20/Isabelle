b26d2318101026053c83383a5e40e36dd22f8c36
Manikaran <mani.singh_123@hotmail.com>
2018-02-15 23:39:21 +0530

    Rene_Descarte's argument
    here in this file i created a function "distinct" which will tell us if the two individual types are distinct or not.

---
+++ Rene_descartes.thy
@@ -0,0 +1,57 @@
+section {* Introduction *}
+
+theory Rene_Descartes
+  imports Main
+begin
+
+text{* This paper presents an automated verification of Rene Descartes's argument on @{ Text "my_mind_and_body_are_distinct"}.
+here, I'm providing the whole argument to make it easy to the user who read this verification. *}
+
+section {* The_Argument *}
+
+text{* I will note that mind differs importantly from body in that body is by it's nature divisible,
+ while mind is indivisible when I think about my mind or in other words Myself in so far,
+I am just a thinking being .I can't distinguish any parts.I understand myself to be a whole 
+unified thing although my whole mind seems united to my whole body.I know cutting off a foot or an
+arm, would not take anything away from my mind and the abilities to will,sense and understand can't
+be called parts that it is one and the same mind that wills,senses and understands on the other hand
+whenever I think of a physical or extended thing, I can mentally divide it
+and therefore understand that Object is divisible. the single fact would be enough to
+teach me that my mind and my body are distinct. *}
+
+typedecl e -- "type of physically existing things"
+text ‹If it can be divided into parts it is divisible›
+
+consts divisible :: "e ⇒ bool" --"predicate symbol for divisibility"
+consts can_be_divided_into_parts :: "e \<Rightarrow> bool"
+consts body :: "e" --"constant symbol for a physical thing"
+prop "divisible(e) ⟶ can_be_divided_into_parts(e)"--"if a physical thing  can_be_divided_into_parts implies it is divisible"
+consts A ::"e"
+consts B ::"e"
+fun distinct :: "divisible(A)::bool \<Rightarrow> divisible(B)::bool ⇒ bool" where
+"distinct True True = True" |
+"distinct _ _ = False"
+consts mind :: "e"
+ 
+theorem Body_is_divisible[simp] : 
+  assumes "divisible(body)"
+  shows "can_be_divided_into_parts(body) \<longrightarrow> divisible(body)"
+proof -
+  {
+  assume "can_be_divided_into_parts(body)"
+  from assms have "divisible(body)" by -
+}
+  then have "can_be_divided_into_parts(body) \<longrightarrow> divisible(body)" by (rule impI)
+  thus ?thesis .
+  qed
+theorem Mind_is_indivisible:
+  assumes "\<not> divisible(mind)"
+  shows "\<not>can_be_divided_into_parts(mind)\<longrightarrow>\<not> divisible(mind)"
+proof -
+  {
+    assume "\<not> can_be_divided_into_parts(mind)"
+  from assms have "\<not> divisible(mind)" by -
+  }
+  then have "\<not> can_be_divided_into_parts(mind) \<longrightarrow> \<not> divisible(mind)" by (rule impI)
+  thus ?thesis .
+qed 
