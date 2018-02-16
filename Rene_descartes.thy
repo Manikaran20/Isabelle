ec17cdaa2405eda5c68de79666a792710745fe1f
Manikaran <mani.singh_123@hotmail.com>
2018-02-17 01:20:27 +0530

    giving divisible function more suitable arguments

--- Rene_descartes.thy
+++ Rene_descartes.thy
@@ -4,7 +4,7 @@ theory Rene_Descartes
   imports Main
 begin
 
-section {*introduction *}
+section {*introduction*}
 
 text{* This paper presents an automated verification of Rene Descartes's argument on @{ Text "my_mind_and_body_are_distinct"}.
 here, I'm providing the whole argument to make it easy to the user who read this verification. *}
@@ -24,20 +24,22 @@ teach me that my mind and my body are distinct. *}
 typedecl e -- "type of physically existing things"
 text ‹If it can be divided into parts it is divisible›
 
-consts divisible :: "e ⇒ bool" --"predicate symbol for divisibility"
+fun divisible :: "e ⇒ bool" where
+"divisible x \<equiv> can_be_divided_into_parts x"
 consts can_be_divided_into_parts :: "e \<Rightarrow> bool"
 consts body :: "e" --"constant symbol for a physical thing"
 prop "divisible(e) ⟶ can_be_divided_into_parts(e)"--"if a physical thing  can_be_divided_into_parts implies it is divisible"
-consts A ::"e"
-consts B ::"e"
-fun distinct :: "bool \<Rightarrow> bool \<rightarrow> bool" where
-"distinct True True = True" |
-"distinct _ _ = False"
 consts mind :: "e"
- 
+
+
+
+fun distinct :: "bool\<times> bool\<Rightarrow> bool" where
+"distinct (True, True) = True" |
+"distinct (_, _) = False"
+
 theorem Body_is_divisible[simp] : 
   assumes "divisible(body)"
-  shows "can_be_divided_into_parts(body) \<longrightarrow> divisible(body)"
+  shows  "can_be_divided_into_parts(body) \<longrightarrow> divisible(body)"
 proof -
   {
   assume "can_be_divided_into_parts(body)"
@@ -57,3 +59,11 @@ proof -
   then have "\<not> can_be_divided_into_parts(mind) \<longrightarrow> \<not> divisible(mind)" by (rule impI)
   thus ?thesis .
 qed 
+datatype A = True 
+datatype B = False 
+definition divisible :: "e\<Rightarrow>bool" where
+"divisible x \<equiv> can_be_divided_into_parts x"
+value "distinct (divisible(mind), divisible(body))"
+
+
+
