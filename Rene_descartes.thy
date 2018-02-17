32c1400e2bd06e3c9c8f37aeab8c8bbac1316f43
Manikaran <mani.singh_123@hotmail.com>
2018-02-17 15:59:27 +0530

    Update Rene_descartes.thy

--- Rene_descartes.thy
+++ Rene_descartes.thy
@@ -1,12 +1,10 @@
-
-
 theory Rene_Descartes
   imports Main
 begin
 
 section {*introduction*}
 
-text{* This paper presents an automated verification of Rene Descartes's argument on @{ Text "my_mind_and_body_are_distinct"}.
+text {* This paper presents an automated verification of Rene Descartes's argument on @{ Text "my_mind_and_body_are_distinct"}.
 here, I'm providing the whole argument to make it easy to the user who read this verification. *}
 
 section {* The_Argument *}
@@ -24,46 +22,15 @@ teach me that my mind and my body are distinct. *}
 typedecl e -- "type of physically existing things"
 text ‹If it can be divided into parts it is divisible›
 
-fun divisible :: "e ⇒ bool" where
-"divisible x \<equiv> can_be_divided_into_parts x"
 consts can_be_divided_into_parts :: "e \<Rightarrow> bool"
-consts body :: "e" --"constant symbol for a physical thing"
-prop "divisible(e) ⟶ can_be_divided_into_parts(e)"--"if a physical thing  can_be_divided_into_parts implies it is divisible"
-consts mind :: "e"
+consts body :: "e" ("body") --"constant symbol for a physical thing"
+consts mind :: "e" ("mind")
 
+prop "can_be_divided_into_parts(body)" --"body can be divided into parts"
+prop "\<not>can_be_divided_into_parts(mind)" --"mind can not be divided into parts"
 
-
-fun distinct :: "bool\<times> bool\<Rightarrow> bool" where
-"distinct (True, True) = True" |
-"distinct (_, _) = False"
-
-theorem Body_is_divisible[simp] : 
-  assumes "divisible(body)"
-  shows  "can_be_divided_into_parts(body) \<longrightarrow> divisible(body)"
-proof -
-  {
-  assume "can_be_divided_into_parts(body)"
-  from assms have "divisible(body)" by -
-}
-  then have "can_be_divided_into_parts(body) \<longrightarrow> divisible(body)" by (rule impI)
-  thus ?thesis .
-  qed
-theorem Mind_is_indivisible:
-  assumes "\<not> divisible(mind)"
-  shows "\<not>can_be_divided_into_parts(mind)\<longrightarrow>\<not> divisible(mind)"
-proof -
-  {
-    assume "\<not> can_be_divided_into_parts(mind)"
-  from assms have "\<not> divisible(mind)" by -
-  }
-  then have "\<not> can_be_divided_into_parts(mind) \<longrightarrow> \<not> divisible(mind)" by (rule impI)
-  thus ?thesis .
-qed 
-datatype A = True 
-datatype B = False 
-definition divisible :: "e\<Rightarrow>bool" where
-"divisible x \<equiv> can_be_divided_into_parts x"
-value "distinct (divisible(mind), divisible(body))"
-
-
-
+  
+function distinct::"e \<Rightarrow> e \<Rightarrow> bool" where
+"distinct x y =(if can_be_divided_into_parts(x)\<and> \<not> can_be_divided_into_parts(y) = False then False else True)"
+   apply(auto)
+end
