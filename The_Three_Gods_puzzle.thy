b3f3e8c5d57e08020864e6dc58807324d7327022
Manikaran <mani.singh_123@hotmail.com>
2018-02-17 22:23:54 +0530

    Update The_Three_Gods_puzzle.thy

--- The_Three_Gods_puzzle.thy
+++ The_Three_Gods_puzzle.thy
@@ -1,4 +1,11 @@
 theory The_Three_Gods_puzzle
-  imports Main
-  begin
-typedecl
+imports Main
+begin
+type_synonym ulu = "bool"
+type_synonym ozo = "bool"
+consts que :: "prop\<Rightarrow>bool"
+consts B :: "prop"
+ prop  "(que(B) = True) \<Rightarrow> ozo"
+function TEE :: "prop \<Rightarrow> bool" where
+"TEE prop = if (prop=True) then ozo else ulu"
+
