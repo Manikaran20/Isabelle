2fbdf1e8dae6ab3f8682dea5fcd7e9c285f72e6a
Manikaran <mani.singh_123@hotmail.com>
2018-02-19 03:46:22 +0530

    Update Rene_descartes.thy

--- Rene_descartes.thy
+++ Rene_descartes.thy
@@ -9,7 +9,7 @@ here, I'm providing the whole argument to make it easy to the user who read this
 
 section {* The_Argument *}
 
-text{*  @ {cite "https://www.youtube.com/watch?v=Din_92eKqW8&t=1457s" I will note that mind differs importantly from body in that body is by it's nature divisible,
+text{*@{ cite "https://www.youtube.com/watch?v=Din_92eKqW8&t=1457s"} I will note that mind differs importantly from body in that body is by it's nature divisible,
  while mind is indivisible when I think about my mind or in other words Myself in so far,
 I am just a thinking being .I can't distinguish any parts.I understand myself to be a whole 
 unified thing although my whole mind seems united to my whole body.I know cutting off a foot or an
@@ -22,17 +22,29 @@ teach me that my mind and my body are distinct. *}
 typedecl e -- "type of physically existing things"
 text ‹If it can be divided into parts it is divisible›
 
-consts breakable :: "e \<Rightarrow> bool"
-consts body :: "e" ("body") 
-consts mind :: "e" ("mind")
+consts breakable ::"e ⇒ bool"
+consts body :: "e" ("body") --"constant symbol for a physical thing"
+consts mind :: "e" ("mind") 
 
 prop "breakable(body)" --"body can be divided into parts"
-prop "\<not> breakable(mind)" --"mind can not be divided into parts"
+prop "\<not>breakable(mind)  " --"mind can not be divided into parts"
+consts P:: "e \<Rightarrow> bool" --"some property for type e"
 
+axiomatization where mind_constrain : "breakable(mind)⟹False"
+
+axiomatization where body_constrain: "breakable(body) ⟹True"
+
+type_synonym σ = "e\<Rightarrow>bool"
+
+abbreviation leib_eq :: "e \<Rightarrow> e \<Rightarrow> σ " (infix "L=" 90) where
+
+"L= x y \<equiv> \<forall> ( λφ. (φx \<longrightarrow> φy))" 
   
-function distinct::"e \<Rightarrow> e \<Rightarrow> bool" where
-"distinct x y =(if breakable(x)\<and> \<not> breakable(y) = False then False else True)"
-   apply(auto)
-   done
-end
- 
+definition  distinct::"e ⇒ e ⇒ bool" where
+"distinct x y iff \<not> L= x y "
+
+
+theorem MyBodyAndMyMind_are_distinct : 
+  assumes 
+  
+end-
