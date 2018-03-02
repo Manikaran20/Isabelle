c07cd0569c08d3fa33d6db524b6c35a45ec7e6ca
Manikaran <mani.singh_123@hotmail.com>
2018-03-02 19:53:40 +0530

    Update Mereology.thy

--- Mereology.thy
+++ Mereology.thy
@@ -18,15 +18,22 @@ axiomatization where axiom_schema:
 "∃x. Φ x ⟶ (∃z. ∀y. ((O y z) ⟷ (φ x ∧ O y x)))"
 
 axiomatization where sum_axiom :
-"Φx = ((x=a)∨(x=b))"
+"Φ x = (x=a ∨ x=b)"
 
 lemma "∃z. (P x z ∧ P y z)⟶(∃z. ∀w. ((O w x) ∨ (O w y)))"
 proof-
-  from axiom_schema sum_axiom have "∃x. (x=a ∨ x=b)⟶(∃z. ∀y. ((O y z)⟷((x=a ∨ x=b)∧ O y x)))" by auto
-  from this have "(∃z. ∀y.(O y z ⟷ ((x=a ∨ x=b)∧ O y x)))" by metis
+  from axiom_schema sum_axiom have "∃x.(x=a ∨ x=b)⟶(∃z.∃x. ∀y. ((O y z)⟷((x=a ∨ x=b)∧ O y x)))" by auto
+  have "Φ x = (x=a ∨ x=b)" using sum_axiom by blast
+  hence "∃x. (x=a ∨ x=b)" by auto
+  from this have "(∃z.∃x.∀y.(O y z ⟷ ((x=a ∨ x=b)∧ O y x)))" using  ‹∃x. (x=a ∨ x=b)⟶(∃z.∀y. ((O y z)⟷((x=a ∨ x=b)∧ O y x)))› by auto
   then have "((x=a ∨ x=b)∧O y x)⟶(O y a ∨ O y b)" by auto
   moreover  have "(∃z. ∀y. ((O y z)⟷(O a y ∨ O b y)))" by metis
   ultimately show "∃z. (P x z ∧ P y z)⟶(∃z. ∀w. ((O w x) ∨ (O w y)))" by auto
 qed
 
 end
+
+
+
+
+
