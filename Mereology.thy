666f7375c09933fb381fcf0d78ff6434845405de
Manikaran <mani.singh_123@hotmail.com>
2018-03-05 03:01:01 +0530

    Update Mereology.thy

--- Mereology.thy
+++ Mereology.thy
@@ -16,6 +16,7 @@ abbreviation overlap :: "μ⇒μ⇒bool" ("O") where
 
 axiomatization where axiom_schema:
 "∃x. Φ x ⟶ (∃z. ∀y. ((O y z) ⟷ (Φ x ∧ O y x)))"
+
 axiomatization where sum_axiom:
  "Φ x = (x=a ∨ x=b)"
 
@@ -29,6 +30,25 @@ proof-
   from this have "∀y.(∃z.((O y z)⟷(O a y ∨ O b y)))" by blast
   hence "∃z. (P x z ∧ P y z)⟶(∃z. ∀w. ((O w x) ∨ (O w y)))" by auto
   show "∃z. (P x z ∧ P y z)⟶(∃z. ∀w. ((O w x) ∨ (O w y)))" by auto
+qed
+axiomatization where product_axiom :
+"Φ x = (P x a ∧ P x b)"
+axiomatization where proper_part :
+"∀x.∃y.(O x y)⟶P x y"
+theorem "∃z.(P x z ∧ P y z)⟶(∃z.(∀w.(P w z ⟷(P w x ∨ P w y))))"
+proof-
+  from product_axiom axiom_schema have "∃x.((P x a ∧ P x b)⟶(∀y.(∃z.(O y z) ⟷((P x a ∧ P x b)∧ O y x))))" by blast
+  have "∃x.(P x a ∧ P x b)" using P_is_reflexive product_axiom by blast
+  hence "(∀y.(∃z.(O y z) ⟷(∃x.(P x a ∧ P x b)∧ O y x)))" using product_axiom by blast
+  from this have "∀y.∃z.(P y z)⟷(∃x.(P x a ∧ P x b)∧ O y x)" using P_is_reflexive by blast
+  from this have "∀y.∃z.(P y z)⟷(∃x.(P x a ∧ P x b)∧ P y x)" by meson
+  from this have "∀y.∃z.(P y z)⟷(∃x.(P x a ∧ P y x)∧(P x b ∧ P y x))" by fastforce
+  from this have "∀y.∃z.(P y z)⟷(P y a ∧ P y b)" by auto
+  then have "O a b" using ‹∃x.(P x a ∧ P x b)› by auto
+  from this have "∃z.(P a z ∧ P b z)" using product_axiom by blast
+  hence "∃z.(P a z ∧ P b z)⟶ (∀y.(∃z.(P y z)⟷(P y a ∧ P y b)))" by auto
+  hence "∃z.(P x z ∧ P y z)⟶(∃z.(∀w.(P w z ⟷(P w x ∨ P w y))))" using product_axiom by blast
+  show "∃z.(P x z ∧ P y z)⟶(∃z.(∀w.(P w z ⟷(P w x ∨ P w y))))" using ‹∃z. P x z ∧ P y z ⟶ (∃z. ∀w. P w z = (P w x ∨ P w y))› by blast
 qed
 
 end
