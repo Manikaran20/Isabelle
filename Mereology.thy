c60f7f2c728e211e4867be8220e89117649836c6
Manikaran <mani.singh_123@hotmail.com>
2018-03-11 04:35:56 +0530

    Update Mereology.thy

--- Mereology.thy
+++ Mereology.thy
@@ -18,15 +18,24 @@ axiomatization where axiom_schema:
 "∃x. Φ x ⟶ (∃z. ∀y. ((O y z) ⟷ (Φ x ∧ O y x)))"
 
 
-lemma "∃z. (P x z ∧ P y z)⟶(∃z. ∀w. ((O w x) ∨ (O w y)))"
+theorem "∃z. (P x z ∧ P y z)⟶(∃z. ∀w. ((O w x) ∨ (O w y)))"
 proof-
-  assume "Φ x = (x=a ∨ x=b)"
-  from this have "∃x.(x=a ∨ x=b)⟶(∃z. ∀y. ((O y z) ⟷ ((x=a ∨ x=b) ∧ O y x)))" using axiom_schema by blast
+  have "∃x.(x=a ∨ x=b)⟶(∃z. ∀y. ((O y z) ⟷ ((x=a ∨ x=b) ∧ O y x)))" using axiom_schema by blast
   then have "∃x.(x=a ∨ x=b)" by auto
   hence "∃x.(∃z. ∀y.((O y z) ⟷ ((x=a ∨ x=b) ∧ O y x)))" by auto
   then have "∀y.(∃x.(x=a ∨ x=b) ∧ O y x)⟶(O y a ∨ O y b)" by auto
   from this have "∀y.(∃z.((O y z)⟷(O a y ∨ O b y)))" by blast
   hence "∃z. (P x z ∧ P y z)⟶(∃z. ∀w. ((O w x) ∨ (O w y)))" by auto
+  show "∃z. (P x z ∧ P y z)⟶(∃z. ∀w. ((O w x) ∨ (O w y)))" by auto
 qed
 
+theorem "∃z.(P z x ∧ P z y)⟶(∃z.(∀w.(P w z ⟷(P w x ∧ P w y))))"
+proof-
+  have "∃x.(P x a ∧ P x b)⟶(∃z.(∀y.(O y z ⟷((P x a ∧ P x b)∧ O y x))))" using axiom_schema by auto
+  from this have "∃x.∀y.(P x a ∧ P x b)∧(O y x)⟶(∀y.(P y a ∧ P y b))" by simp
+  hence "∃x.(P x a ∧ P x b)⟶(∃z.(∀y.(O y z ⟷(P y a ∧ P y b))))" using P_is_reflexive by blast
+  have "∀y.∃z.(O y z)⟶P y z" using P_is_reflexive by blast
+  hence "∃x.(P x a ∧ P x b)⟶(∃z.(∀y.(P y z ⟷(P y a ∧ P y b))))" by auto
+  thus "∃z.(P z x ∧ P z y)⟶(∃z.(∀w.(P w z ⟷(P w x ∧ P w y))))" by auto
+qed
 end
